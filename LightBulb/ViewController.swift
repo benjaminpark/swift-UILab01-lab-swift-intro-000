dc2f4c28eb4c290149c5b7c0731b3efe357e4e8a
Ben Park <benjaminpark@live.com>
2018-04-27 15:30:44 -0400

    Lol super easy, but learn the switch case

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -17,12 +17,25 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         
-        lightBulb.backgroundColor = UIColor.blue
+        lightBulb.backgroundColor = UIColor.green
         
     }
     
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            lightBulb.backgroundColor = UIColor.red
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            lightBulb.backgroundColor = UIColor.yellow
+        }
+        else if sender.selectedSegmentIndex == 2 {
+            lightBulb.backgroundColor = UIColor.blue
+        }
+        else if sender.selectedSegmentIndex == 3 {
+            lightBulb.backgroundColor = UIColor.green
+        }
         
         print(sender.selectedSegmentIndex)
         
